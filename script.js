7cf347ecd29e61a135b98a55bd665b28b58e21e6
sravantivanjarapu <sravantivanjarapu@gmail.com>
2024-11-28 22:44:30 +0530

    successfully

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll to sections
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth'
+    });
+}
